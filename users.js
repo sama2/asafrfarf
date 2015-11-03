ab76b48e8352de6fc36853cec523640b0e6d6c1e
GHFBBS <christian_pa_2001@hotmail.com>
2015-11-03 21:10:45 +0100

    Update users.js

--- users.js
+++ users.js
@@ -736,7 +736,7 @@ User = (function () {
 			name = Config.namefilter(name, this);
 		}
 		name = Tools.getName(name);
-		//name = name.replace(/^[^A-Za-z0-9]+/, "");
+		name = name.replace(/^[^A-Za-z0-9]+/, "");
 		return name;
 	};
 	/**
